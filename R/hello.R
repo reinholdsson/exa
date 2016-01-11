e238e4a14b8334fd11981e9c444d52f1cd9d9ca9
Thomas Reinholdsson <reinholdsson@gmail.com>
2016-01-11 13:16:30 +0100

    update desc and add query fun

--- R/hello.R
+++ R/hello.R
@@ -2,8 +2,8 @@
 #'
 #' ...
 #'
-exasol <- function(user = 'sys', password = 'exasol', host = 'localhost', port = 8563L) {
-
+#' @export
+exasol <- function(user = 'sys', password = 'exasol', host = 'localhost', port = 8563L, ...) {
   args <- as.list(environment())
 
   # ODBC Driver
@@ -11,13 +11,19 @@ exasol <- function(user = 'sys', password = 'exasol', host = 'localhost', port =
     system.file(package = packageName()),
     switch(
       Sys.info()['sysname'],
-
-      # Mac OS
-      'Darwin' = 'odbc/lib/darwin/x86_64/libexaodbc-io418sys.dylib'
+      'Darwin' = 'odbc/lib/darwin/x86_64/libexaodbc-io418sys.dylib'  # Mac OS
     )
   )
 
   conn_string <- infuse('Driver={{driver}};UID={{user}};PWD={{password}};EXAHOST={{host}}:{{port}}', args)
-  odbcDriverConnect(conn_string)
-
+  odbcDriverConnect(conn_string, ...)
 }
+
+#' @export
+query <- function(conn, sql, ...) {
+  dots <- list(...)
+  q <- if (length(dots) == 0) infuse(sql, NULL) else infuse(sql, ...)
+  sqlQuery(conn, q)
+}
+
+
