394385eeafdbb0135a8376ce6d830c0699accc09
Thomas Reinholdsson <reinholdsson@gmail.com>
2016-08-18 01:50:51 +0200

    simplify package

--- R/exa.R
+++ R/exa.R
@@ -1,42 +1,20 @@
-#' Exasol JDBC Connection
-#'
-#' ...
-#'
 #' @export
-jdbc <- function(args = NULL, host, user = 'sys', password, schema = 'public', ...) {
-  args <- merge_list(args, as.list(environment()))
-
-  jdbcDriver <- JDBC(
+jdbc <- function(...) {
+  driver <- JDBC(
     driverClass = 'com.exasol.jdbc.EXADriver',
     classPath = file.path(system.file(package = 'exa'), 'jdbc/exajdbc.jar'),
     identifier.quote = '\"'
   )
-
-  jdbcConnection <- dbConnect(
-    jdbcDriver,
-    infuse("jdbc:exa:{{host}};schema={{schema}};user={{user}};password={{password}}", args)
+  con <- dbConnect(
+    driver,
+    infuse('jdbc:exa:{{host}};schema={{schema|public}};user={{user|sys}};password={{password}}', list(...))
   )
-
-  return(jdbcConnection)
+  return(con)
 }
-
 
 #' @export
 query <- function(con, sql, ...) {
-  require(data.table)
   dots <- list(...)
   q <- if (length(dots) == 0) infuse(sql, NULL) else infuse(sql, ...)
-  res <-
-    if ('JDBCConnection' %in% class(con)) RJDBC::dbGetQuery(con, q)
-    else stop('Unsupported connection')
-  data.table(res)
-}
-
-merge_list <- function (x, y, ...){
-  if (length(x) == 0) return(y)
-  if (length(y) == 0) return(x)
-  i = match(names(y), names(x))
-  i = is.na(i)
-  if (any(i)) x[names(y)[which(i)]] = y[which(i)]
-  return(x)
+  RJDBC::dbGetQuery(con, q)
 }
